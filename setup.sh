31d57f600d13db43add8b3a9235ef6c684922857
sandstroh <sandstroh@gmail.com>
2016-12-03 19:10:24 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -11,4 +11,19 @@ fi
 cp nginx/static.page /etc/nginx/site-enabled/
 nginx -s reload
 
-cp www/ var/www/static/
+WEBSITE_DIRECTORY=/var/www/netsec/
+
+if [ -d "$WEBSITE_DIRECTORY" ]
+then
+    rm -rf $WEBSITE_DIRECTORY
+fi
+
+cp -r www/css/ $WEBSITE_DIRECTORY/css
+cp -r www/fonts/ $WEBSITE_DIRECTORY/fonts
+cp -r www/images/ $WEBSITE_DIRECTORY/images
+cp -r www/js $WEBSITE_DIRECTORY/js
+cp www/netsec.html $WEBSITE_DIRECTORY/netsec.html
+
+cp www/index.cgi /usr/lib/cgi-bin/index.cgi
+chmod +x /usr/lib/cgi-bin/index.cgi
+
