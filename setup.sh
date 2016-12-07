cf8d2c1b3bb8d44a557a774aebc02f18d9f1642c
netsec <netsec@netsec.home>
2016-12-07 13:50:26 +0100

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -19,13 +19,16 @@ then
     rm -rf $WEBSITE_DIRECTORY
 fi
 
+mkdir -p $WEBSITE_DIRECTORY
+
+
 cp -r www/css/ $WEBSITE_DIRECTORY/css
 cp -r www/fonts/ $WEBSITE_DIRECTORY/fonts
 cp -r www/images/ $WEBSITE_DIRECTORY/images
 cp -r www/js $WEBSITE_DIRECTORY/js
-cp www/netsec.html $WEBSITE_DIRECTORY/netsec.html
+cp www/netsec.html $WEBSITE_DIRECTORY/
 chown -R www-data:www-data /var/www
 
-cp www/index.cgi /usr/lib/cgi-bin/index.cgi
+cp www/index.cgi /usr/lib/cgi-bin/
 chmod +x /usr/lib/cgi-bin/index.cgi
 
